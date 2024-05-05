6910c858f1ecac52ad94fdc43ebd1b315ba42ebe
luckx <149601518+luckx123001@users.noreply.github.com>
2024-05-05 14:57:46 +0800

    Update ModifierFunctionTest.sol

--- ModifierFunctionTest.sol
+++ ModifierFunctionTest.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 
 contract ModifierFunctionTest {
     bool private paused; //默认是false
-
+    //public 状态变量会默认生成同名的函数，供外界访问，此时为：function count() external returns(uint) { return count;}
     uint public count;
 
     function setPaused(bool _paused) external {
@@ -29,6 +29,7 @@ contract ModifierFunctionTest {
 
     modifier whenNotPaused() {
         require(!paused, "paused");
+        //表示校验通过，函数继续执行
         _;
     }
 
